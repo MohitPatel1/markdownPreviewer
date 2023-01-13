746440048c6b71efdfd6634220b1543e75e2e08e
krishna404 <agarwalbalkrishnaindia@gmail.com>
2023-01-13 14:21:25 +0530

    converted to latest react syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { marked } from 'marked';
+import { render } from 'react-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState("");
+
+  const updateMarkdownState = (input) => {
+    setMarkdown(input);
+  };
+
+  const textAreaStyles = {
+    height: "50vh"
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-center m-4">Markdown Editor</h1>
+          <div className="flex m-2">
+            <h2 className="text-center w-2/4">Markdown Input </h2>
+            <h2 className="text-center w-2/4">Preview</h2>
+          </div>
+          <div className="flex">
+            <textarea className="w-2/4 m-4 bg-green-300" value={markdown} onChange={(e) => {updateMarkdownState(e.target.value)}} style={textAreaStyles}></textarea>
+            <div className="w-2/4 m-4 bg-blue-300" style={textAreaStyles}>
+              <div dangerouslySetInnerHTML = {{__html: marked(markdown)}} ></div>
+            </div>
+          </div>
     </div>
   )
 }
